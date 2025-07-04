d6861bb3091fc628d1bddfcb9560dd6b97eacc73
Vital Fadeev <vital.fadeev@gmail.com>
2025-07-04 20:24:48 +0700

    upd

--- source/app.d
+++ source/app.d
@@ -10,17 +10,35 @@ main () {
 	foreach (event; LibInput (null))
 		switch (event.type) {
 			case LIBINPUT_EVENT_DEVICE_ADDED:
-				writefln ("%s: %s added", event.type.to!string, event.device.name.to!string); break;
+				writefln ("%s: %s added", 
+					event.type.to!string, 
+					event.device.name.to!string); break;
 			case LIBINPUT_EVENT_KEYBOARD_KEY:
-				writefln ("%s: %d: %s", event.type.to!string, event.keyboard.get_key, event.keyboard.get_key_state.to!string); break;
+				writefln ("%s: %d: %s", 
+					event.type.to!string, 
+					event.keyboard.get_key, 
+					event.keyboard.get_key_state.to!string); break;
 			case LIBINPUT_EVENT_POINTER_MOTION:
-				writefln ("%s: dx,dy: %f,%f", event.type.to!string, event.pointer.dx, event.pointer.dy); break;
+				writefln ("%s: dx,dy: %f,%f", 
+					event.type.to!string, 
+					event.pointer.dx, 
+					event.pointer.dy); break;
 			case LIBINPUT_EVENT_POINTER_MOTION_ABSOLUTE:
-				writefln ("%s: abs_x,abx_y: %f,%f", event.type.to!string, event.pointer.absolute_x, event.pointer.absolute_y); break;
+				writefln ("%s: abs_x,abx_y: %f,%f", 
+					event.type.to!string, 
+					event.pointer.absolute_x, 
+					event.pointer.absolute_y); break;
 			case LIBINPUT_EVENT_POINTER_BUTTON:
-				writefln ("%s: %d %s", event.type.to!string, event.pointer.button, event.pointer.button_state.to!string); break;
+				writefln ("%s: %d %s", 
+					event.type.to!string, 
+					event.pointer.button, 
+					event.pointer.button_state.to!string); break;
 			case LIBINPUT_EVENT_POINTER_AXIS:
-				writefln ("%s: axe_source: %s: x,y: %f,%f", event.type.to!string, event.pointer.axis_source.to!string, event.pointer.axis_value (LIBINPUT_POINTER_AXIS_SCROLL_HORIZONTAL), event.pointer.axis_value (LIBINPUT_POINTER_AXIS_SCROLL_VERTICAL)); break;
+				writefln ("%s: axe_source: %s: x,y: %f,%f", 
+					event.type.to!string, 
+					event.pointer.axis_source.to!string, 
+					event.pointer.axis_value (LIBINPUT_POINTER_AXIS_SCROLL_HORIZONTAL), 
+					event.pointer.axis_value (LIBINPUT_POINTER_AXIS_SCROLL_VERTICAL)); break;
 			case LIBINPUT_EVENT_TOUCH_DOWN:
 			case LIBINPUT_EVENT_TOUCH_UP:
 			case LIBINPUT_EVENT_TOUCH_MOTION:
